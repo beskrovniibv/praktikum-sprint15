e2e47027c787bdaa9d1b724f74ffed6d6c9972db
Борис Бескровный <beskrovniibv@yandex.ru>
2023-05-03 00:56:27 +0500

    fix f.a-6.py performance issue

--- f.a-6.py
+++ f.a-6.py
@@ -5,23 +5,23 @@ def solve():
     source = input().split()
     ans = ''
     zero_index, index = 0, 0
-    z_l, z_r = None, None
+    z_l, z_r = -1, -1
     while zero_index < n:
         while source[zero_index] != '0':
             zero_index += 1
             if zero_index == n:
                 break
-        if z_r is not None:
+        if z_r > -1:
             z_l = z_r
         if zero_index == n:
-            z_r = None
+            z_r = -1
         else:
             z_r = zero_index
-        index = z_l + 1 if z_l is not None else 0
+        index = z_l + 1 if z_l > -1 else 0
         while index < zero_index:
-            if z_l is None:
+            if z_l == -1:
                 ans += f'{z_r - index} '
-            elif z_r is None:
+            elif z_r == -1:
                 ans += f'{index - z_l} '
             else:
                 v1 = z_r - index
@@ -31,7 +31,7 @@ def solve():
                 else:
                     ans += f'{v2} '
             index += 1
-        if z_r is not None:
+        if z_r > -1:
             ans += '0 '
         zero_index += 1
     print(ans)
